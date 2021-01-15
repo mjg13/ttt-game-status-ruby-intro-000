9d934c14966da4a6cf1e310eb71acd3187f4601b
Mary Jo Gray <mj81000@gmail.com>
2021-01-15 15:24:40 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def winning_combo?(board, win_combo, token)
+  win_combo.all? do |spot|
+    board[spot] == token
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    winning_combo?(board, win_combo, 'X') || winning_combo?(board, win_combo, 'O')
+  end
+end
